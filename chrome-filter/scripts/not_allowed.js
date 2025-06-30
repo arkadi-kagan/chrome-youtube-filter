309ec4f9ff6dd864bf99c77e70531622e2e92db9
Arkadi Kagan <arkadi_kagan@hotmail.com>
2025-06-30 17:32:27 +0300

    More words and block explicit sites

---
+++ chrome-filter/scripts/not_allowed.js
@@ -0,0 +1,4 @@
+	let bodies = document.getElementsByTagName("body");
+	for (let i = 0; i < bodies.length; i++) {
+		bodies[i].setHTMLUnsafe("<body><H1>תוכן אסור בעמוד, לא נמצא מקום ספציפי</H1><BR/><p>נסה יוטיו: <a href=\"https://www.youtube.com\">Youtube</a></p></body>")
+	}

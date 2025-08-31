b3fcd1b2461e5443c49dfcab061d1871ef4f2791
Arkadi Kagan <arkadi_kagan@hotmail.com>
2025-08-31 23:07:04 +0300

    Update not_allowed.js

--- chrome-filter/scripts/not_allowed.js
+++ chrome-filter/scripts/not_allowed.js
@@ -1,9 +1,9 @@
 let bodies = document.getElementsByTagName("body");
 for (let i = 0; i < bodies.length; i++) {
-	bodies[i].setHTMLUnsafe("<body><H1>אתר לא מורשה</H1><BR/><p>נסה יוטיוב: <a href=\"https://www.youtube.com\">Youtube</a></p></body>");
+	bodies[i].outerHTML = "<body><H1>אתר לא מורשה</H1><BR/><p>נסה יוטיוב: <a href=\"https://www.youtube.com\">Youtube</a></p></body>";
 }
 
 let heads = document.getElementsByTagName("head");
 for (let i = 0; i < heads.length; i++) {
-	heads[i].setHTMLUnsafe("<head><title>אתר לא מורשה</title></head>");
+	heads[i].outerHTML = "<head><title>אתר לא מורשה</title></head>";
 }

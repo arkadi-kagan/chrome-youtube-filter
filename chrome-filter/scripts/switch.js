42649a22b91d4bb4f0f8362bcd0548722e0053ac
Arkadi Kagan <arkadi_kagan@hotmail.com>
2026-04-06 08:19:45 +0300

    Do filter all the rest of sites

--- chrome-filter/scripts/switch.js
+++ chrome-filter/scripts/switch.js
@@ -1,13 +1,15 @@
 function cleanup_by_filter() {
     hostname = window.location.hostname;
-    if (hostname.match(/^[a-z0-9]+\.(vk|ok|pornhub|xvideos|xnxx|rutube|yandex|dzen|x|mover|spotify|eminem|reddit|tiktok|tlgrm|kinogo)\.[a-z0-9]+/i)) {
-        ban_whole_page();
-    } else if (hostname.match(/music\.youtube\.[a-z0-9\.]+/i)) {
+    if (hostname.match(/^[a-z0-9]+\.(vk|ok|pornhub|xvideos|xnxx|rutube|x|mover|eminem|reddit|tiktok|tlgrm|kinogo)\.[a-z0-9]+/i)) {
         ban_whole_page();
     } else if (hostname.match(/^[a-z]+\.youtube\.[a-z0-9\.]+/i)) {
         cleanup_youtube();
     } else if (hostname.match(/^[a-z]+\.google\.[a-z0-9\.]+/i)) {
         cleanup_google();
+    } else {
+        filter_by_tag("script");
+        filter_by_tag("dev");
+        filter_last_resort();
     }
 }
 
